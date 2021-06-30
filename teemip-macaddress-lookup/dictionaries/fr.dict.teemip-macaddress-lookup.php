9e7155f50f3ed0db1b0568c31d2da1e4eabd1432
xtophe38 <christophe.naud@gmail.com>
2021-06-30 11:41:52 +0200

    Create new version for iTop 3.0. Extend compliance with MVC architecture. Implement twig.

--- teemip-macaddress-lookup/dictionaries/fr.dict.teemip-macaddress-lookup.php
+++ teemip-macaddress-lookup/dictionaries/fr.dict.teemip-macaddress-lookup.php
@@ -16,17 +16,27 @@
 Dict::Add('FR FR', 'French', 'Français', array(
 	'Menu:MACLookup' => 'Recherche d\'adresse MAC',
 	'Menu:MACLookup+' => 'Recherche d\'adresse MAC',
+
+	'teemip-macaddress-lookup/Operation:MACAddressLookup/Title' => 'Recherche d\'adresse MAC',
+	'teemip-macaddress-lookup/Operation:DoMACAddressLookup/Title' => 'Recherche d\'adresse MAC',
+	'teemip-macaddress-lookup/Operation:MACAddressLookupFromCI/Title' => 'Recherche d\'adresse MAC',
+
 	'UI:MACLookup:Action:CI:Lookup' => 'Recherche d\'adresse MAC',
-	'UI:MACLookup:Action:CI:Lookup:Title' => '%1$s: <span class="hilite">%2$s</span> - Résultat de la recherche d\'adresse MAC',
 	'UI:MACLookup:Action:CI:Details' => 'Détails',
+	'UI:MACLookup:Action:CI:NoMAC' => 'Le CI n\'a aucune adresse MAC ou aucune n\'est renseignée !',
+
 	'UI:MACLookup:Action:Lookup:Title' => 'Recherche d\'adresse MAC',
+	'UI:MACLookup:Action:Lookup:Helper' => 'Sélectionez une adresse ou un préfixe',
 	'UI:MACLookup:Action:Lookup:SelectMACAddress' => 'Adresse MAC',
 	'UI:MACLookup:Action:Lookup:SelectMACPrefix' => 'Préfixe',
+
 	'UI:MacLookup:Action:DoLookup:CannotRun:EmptyMAC' => 'Aucune adresse MAC ni préfixe n\'ont été renseignés !',
 	'UI:MacLookup:Action:DoLookup:HasError' => 'La recherche d\'adresse MAC a échoué: ',
 	'UI:MACLookup:Action:DoLookup:Result' => 'Adresse MAC - Résultat de recherche',
 	'UI:MACLookup:Action:DoLookup:Result:Attribute' => 'Attribut',
+	'UI:MACLookup:Action:DoLookup:Result:MACAddressTitle' => '%1$s',
 	'UI:MACLookup:Action:DoLookup:Result:MACAddress' => 'Adresse MAC',
+	'UI:MACLookup:Action:DoLookup:Result:MACPrefixTitle' => 'Prefix %1$s',
 	'UI:MACLookup:Action:DoLookup:Result:MACPrefix' => 'Préfixe',
 	'UI:MACLookup:Action:DoLookup:Result:Company' => 'Entreprise',
 	'UI:MACLookup:Action:DoLookup:Result:Address' => 'Adresse',
