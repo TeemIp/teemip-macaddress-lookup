c2e7991b076811a5c442461ee7781c8d5c5b4b70
xtophe38 <christophe.naud@gmail.com>
2021-08-03 18:27:48 +0200

    Continuation of modifications to fit both 2.7 and 3.0 branches.

--- teemip-macaddress-lookup/vendor/composer/autoload_classmap.php
+++ teemip-macaddress-lookup/vendor/composer/autoload_classmap.php
@@ -6,8 +6,7 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
-    'Composer\\InstalledVersions' => $vendorDir . '/composer/InstalledVersions.php',
-    'TeemIp\\TeemIp\\Extension\\MACAddressLookup\\Controller\\MacLookupController' => $baseDir . '/src/Controller/MacLookupController.php',
-    'TeemIp\\TeemIp\\Extension\\MACAddressLookup\\Hook\\MacLookupOtherActions' => $baseDir . '/src/Hook/MacLookupOtherActions.php',
-    'TeemIp\\TeemIp\\Extension\\MACAddressLookup\\Service\\MacLookupReport' => $baseDir . '/src/Service/MacLookupReport.php',
+	'TeemIp\\TeemIp\\Extension\\MACAddressLookup\\Controller\\MacLookupController' => $baseDir.'/src/Controller/MacLookupController.php',
+	'TeemIp\\TeemIp\\Extension\\MACAddressLookup\\Hook\\MacLookupOtherActions' => $baseDir.'/src/Hook/MacLookupOtherActions.php',
+	'TeemIp\\TeemIp\\Extension\\MACAddressLookup\\Service\\MacLookupReport' => $baseDir.'/src/Service/MacLookupReport.php',
 );
