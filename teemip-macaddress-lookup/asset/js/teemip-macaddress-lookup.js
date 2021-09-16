f8ec49c0f3ff65c97b299544e8f929ef0be3f422
xtophe38 <christophe.naud@gmail.com>
2021-09-16 17:48:26 +0200

    Add dictionary entries to be used within forms

---
+++ teemip-macaddress-lookup/asset/js/teemip-macaddress-lookup.js
@@ -0,0 +1,17 @@
+/*
+ * @copyright   Copyright (C) 2021 TeemIp
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
+
+/* This is a workaround to load the following dictionary entries in the page.
+ *  These are normally loaded trough a $oPage->add_dict_entry() in cmdbabstract class
+ */
+var aTeemIpDictEntries = {
+	"UI:ValueInvalidFormat": 'Invalid format',
+	"UI:ValueMustBeSet": "Please specify a value",
+	"UI:ValueMustBeChanged": "Please change the value"
+};
+
+if ((typeof Dict != "undefined") && (typeof Dict._entries != "undefined")) {
+	$.extend(Dict._entries, aTeemIpDictEntries);
+}
