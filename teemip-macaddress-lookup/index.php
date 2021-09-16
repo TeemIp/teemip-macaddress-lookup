2ba56e491f73cb5f5f2eb8b21aaa0a17575a0586
xtophe38 <christophe.naud@gmail.com>
2021-09-16 17:48:59 +0200

    Add js files to the controller

--- teemip-macaddress-lookup/index.php
+++ teemip-macaddress-lookup/index.php
@@ -8,12 +8,14 @@ namespace TeemIp\TeemIp\Extension\MACAddressLookup;
 
 use ContextTag;
 use TeemIp\TeemIp\Extension\MACAddressLookup\Controller\MacLookupController;
+use utils;
 
 require_once(APPROOT.'application/startup.inc.php');
 $oCtxMACLookup = new ContextTag(ContextTag::TAG_SETUP);
 
 $oMacLookupController = new MacLookupController(MODULESROOT.'teemip-macaddress-lookup/templates', 'teemip-macaddress-lookup');
 $oMacLookupController->SetDefaultOperation('MACAddressLookup');
+$oMacLookupController->AddLinkedScript(utils::GetAbsoluteUrlModulesRoot().'teemip-macaddress-lookup/asset/js/teemip-macaddress-lookup.js');
 $oMacLookupController->HandleOperation();
 
 unset($oCtxMACLookup);
