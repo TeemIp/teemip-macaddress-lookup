1584a60b160ef9d18d66e993193d156fef58df63
xtophe38 <christophe.naud@gmail.com>
2021-12-22 10:59:34 +0100

    Last modifs before 3.0 release

---
+++ teemip-macaddress-lookup/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '132bc57d2428387facfebb0f1cd242432a534dfa',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '132bc57d2428387facfebb0f1cd242432a534dfa',
+            'dev_requirement' => false,
+        ),
+    ),
+);
