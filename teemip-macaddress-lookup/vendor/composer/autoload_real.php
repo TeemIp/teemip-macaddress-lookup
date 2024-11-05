134dad98fa58d7f83b7365f58d5f678b5ab4c727
xtophe38 <christophe.naud@gmail.com>
2024-11-05 11:24:44 +0100

    Update composer

--- teemip-macaddress-lookup/vendor/composer/autoload_real.php
+++ teemip-macaddress-lookup/vendor/composer/autoload_real.php
@@ -23,20 +23,11 @@ class ComposerAutoloaderInit330784e6328c3f366799fa517676032b
         }
 
         spl_autoload_register(array('ComposerAutoloaderInit330784e6328c3f366799fa517676032b', 'loadClassLoader'), true, true);
-        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
+        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInit330784e6328c3f366799fa517676032b', 'loadClassLoader'));
 
-        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
-        if ($useStaticLoader) {
-            require __DIR__ . '/autoload_static.php';
-
-            call_user_func(\Composer\Autoload\ComposerStaticInit330784e6328c3f366799fa517676032b::getInitializer($loader));
-        } else {
-            $classMap = require __DIR__ . '/autoload_classmap.php';
-            if ($classMap) {
-                $loader->addClassMap($classMap);
-            }
-        }
+        require __DIR__ . '/autoload_static.php';
+        call_user_func(\Composer\Autoload\ComposerStaticInit330784e6328c3f366799fa517676032b::getInitializer($loader));
 
         $loader->setClassMapAuthoritative(true);
         $loader->register(true);
